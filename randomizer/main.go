a6b3c46f8c769951eca65131c90a0f1a0ba7da42
Thomas Maurice <thomas@maurice.fr>
2021-07-11 02:37:44 +0100

    makes the randomizer better

--- randomizer/main.go
+++ randomizer/main.go
@@ -12,17 +12,34 @@ import (
 	ringo "github.com/thomas-maurice/ringos/go-client"
 )
 
+const (
+	Red   = 0
+	Green = 1
+	Blue  = 2
+)
+
 var (
-	address string
-	debug   bool
-	Red     = 0
-	Green   = 1
-	Blue    = 2
+	address         string
+	debug           bool
+	changeDirection bool
+
+	// Colour random changes
+	ColourChangeChance = 5
+	ColourChangeMax    = 10
+
+	// Speed random changes
+	SpeedChangeChance = 5
+	SpeedChangeMax    = 10
+
+	// Direction random changes
+	DirectionChangeChance = 18
+	DirectionMax          = 20
 )
 
 func init() {
 	flag.StringVar(&address, "address", "http://10.99.4.102", "Address of the device")
 	flag.BoolVar(&debug, "debug", false, "Debug mode ?")
+	flag.BoolVar(&changeDirection, "chg-dir", false, "Randomly change direction ?")
 
 	rand.Seed(time.Now().UnixNano())
 }
@@ -33,27 +50,51 @@ func randomize() (string, *ringo.ColourRequest, interface{}) {
 	var R int
 	var G int
 	var B int
+	var chase ringo.ChaseRequest
+	var clr *ringo.ColourRequest
 
 	switch colour {
 	case 0:
-		R = 255
+		R = int(rand.Int()%128) + 128
 		G = rand.Int() % 255
 		B = rand.Int() % 255
 	case 1:
 		R = rand.Int() % 255
-		G = 255
+		G = int(rand.Int()%128) + 128
 		B = rand.Int() % 255
 	case 2:
 		R = rand.Int() % 255
 		G = rand.Int() % 255
-		B = 255
+		B = int(rand.Int()%128) + 128
 	}
 
 	c := fmt.Sprintf("%02x%02x%02x", R, G, B)
 
-	return "chase", &ringo.ColourRequest{
-		Colour: c,
-	}, nil
+	if rand.Int()%ColourChangeMax >= ColourChangeChance {
+		clr = &ringo.ColourRequest{
+			Colour: c,
+			Mode:   ringo.MODE_CHASE,
+		}
+	}
+
+	if rand.Int()%SpeedChangeMax >= SpeedChangeChance {
+		chase.Speed = int64(rand.Int() % 10)
+	}
+
+	if changeDirection && rand.Int()%DirectionMax >= DirectionChangeChance {
+		switch rand.Int() % 2 {
+		case 1:
+			chase.Direction = 1
+			break
+		case 0:
+			chase.Direction = -1
+			break
+		default:
+			panic("nope")
+		}
+	}
+
+	return "chase", clr, &chase
 }
 
 func main() {
@@ -105,9 +146,11 @@ func main() {
 			case "breathing":
 				break
 			case "chase":
-				_, err := client.SetColour(colour)
-				if err != nil {
-					panic(err)
+				if colour != nil {
+					_, err := client.SetColour(colour)
+					if err != nil {
+						panic(err)
+					}
 				}
 				if data != nil {
 					chaseData, ok := data.(*ringo.ChaseRequest)
